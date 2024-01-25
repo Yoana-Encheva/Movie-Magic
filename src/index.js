a288d4f613659e02ab1435138bfc44aa55259e36
Yoana-Encheva <je.encheva@gmail.com>
2024-01-25 20:08:39 +0200

    Add and config Handlebars

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const router = require("express").Router();
 
 const configExpress = require("./config/configExpress");
+const configHandlebars = require("./config/configHandlebars");
 
 const app = express();
 const port = 3000;
 
 configExpress(app);
+configHandlebars(app);
 
-app.get("/", (req, res) => {
-  res.status(200);
+router.get("/", async (req, res) => {
+  res.render("home");
 });
+
+app.use(router);
 
 app.listen(port, () => console.log(`Express running on port:  ${port}...`));
