636b1c770bf90d20973245e025d6732cb2ea84dc
Yoana-Encheva <je.encheva@gmail.com>
2024-01-26 16:19:18 +0200

    Add custom handlebars helper for movie rating

--- src/config/configHandlebars.js
+++ src/config/configHandlebars.js
@@ -1,11 +1,16 @@
 const handlebars = require("express-handlebars");
 const path = require("path");
 
+function movieRating(rating) {
+  return "&#x2605;".repeat(Number(rating));
+}
+
 function configHandlebars(app) {
   app.engine(
     "hbs",
     handlebars.engine({
       extname: "hbs",
+      helpers: { movieRating },
     })
   );
   app.set("view engine", "hbs");
