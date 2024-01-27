e867ea565f74424e752ed8868e9d691ce9bfa6ca
Yoana-Encheva <je.encheva@gmail.com>
2024-01-27 17:02:55 +0200

    Add mongoose

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const routes = require("./routes");
+const mongoose = require("mongoose");
 
 const configExpress = require("./config/configExpress");
 const configHandlebars = require("./config/configHandlebars");
+
+const routes = require("./routes");
 
 const app = express();
 const port = 3000;
@@ -12,4 +14,11 @@ configHandlebars(app);
 
 app.use(routes);
 
-app.listen(port, () => console.log(`Express running on port:  ${port}...`));
+mongoose
+  .connect(`mongodb://localhost:27017/magic-movies`)
+  .then(() => {
+    console.log("DB connected");
+
+    app.listen(port, () => console.log(`Express running on port:  ${port}...`));
+  })
+  .catch((err) => console.log("Can not connect to DB", err));
