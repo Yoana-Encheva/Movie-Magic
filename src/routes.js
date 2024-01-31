0036763fc77a09d4008f661acacc4c6c57ec056f
Yoana-Encheva <je.encheva@gmail.com>
2024-01-31 14:50:03 +0200

    Add create cast page and controller

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 
 const staticPagesController = require("./controllers/staticPages");
 const moviesController = require("./controllers/movie");
+const castController = require("./controllers/cast");
 
 router.use(staticPagesController);
 router.use(moviesController);
+router.use("/cast", castController);
 
 router.get("*", (req, res) => {
   res.redirect("/404");
