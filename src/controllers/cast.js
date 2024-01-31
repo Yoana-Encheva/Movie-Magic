a1e0dc290fb549e33a50039e91996967eb382797
Yoana-Encheva <je.encheva@gmail.com>
2024-01-31 15:32:57 +0200

    Add post action for cast create

--- src/controllers/cast.js
+++ src/controllers/cast.js
@@ -4,4 +4,11 @@ router.get("/create", async (req, res) => {
   res.render("cast/create");
 });
 
+router.post("/create", async (req, res) => {
+  const body = req.body;
+
+  console.log(body);
+  res.redirect("/");
+});
+
 module.exports = router;
